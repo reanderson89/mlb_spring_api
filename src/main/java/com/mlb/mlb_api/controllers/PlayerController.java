f67f9f83a9b3f7f1b2bb66b3da5e344a5753f64d
robAnderson <reanderson89@gmail.com.com>
2023-06-30 11:08:50 -0700

    completed players routes

--- src/main/java/com/mlb/mlb_api/controllers/PlayerController.java
+++ src/main/java/com/mlb/mlb_api/controllers/PlayerController.java
@@ -1,24 +1,66 @@
 package com.mlb.mlb_api.controllers;
 
 import com.mlb.mlb_api.entities.Player;
-import com.mlb.mlb_api.repositories.PlayerRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mlb.mlb_api.services.PlayerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
 
-    private final PlayerRepository playerRepository;
+    private final PlayerService playerService;
 
-    public PlayerController(PlayerRepository playerRepository) {
-        this.playerRepository = playerRepository;
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
     }
 
-    @PostMapping("/add")
-    public Player addPlayer(@RequestBody Player player){
-        return playerRepository.save(player);
+    @GetMapping
+    public ResponseEntity<Iterable<Player>> findAllPlayers(){
+        return ResponseEntity.ok(playerService.findAllPlayers());
+//        return ResponseEntity.status(HttpStatus.OK).body(playerService.findAllPlayers());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Player> getPlayerById(@PathVariable Integer id){
+        return ResponseEntity.ok(playerService.getPlayerById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Player> addPlayer(@RequestBody Player player){
+        Player savedPlayer = playerService.addPlayer(player);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/players/{id}")
+                .buildAndExpand(savedPlayer.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(player);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Player>  updatedPlayer(@PathVariable Integer id, @RequestBody Player updates){
+        return ResponseEntity.ok(playerService.updatePlayer(id, updates));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HashMap<String, Object>> deletePlayer(@PathVariable Integer id){
+
+        HashMap<String, Object> responseMap = playerService.deletePlayer(id);
+
+        if(responseMap.get("playerInfo") == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMap);
+        }
+
+        return ResponseEntity.ok(responseMap);
+    }
+
+
+
+
 }
---
+++ src/main/java/com/mlb/mlb_api/services/PlayerService.java
@@ -0,0 +1,77 @@
+package com.mlb.mlb_api.services;
+
+import com.mlb.mlb_api.entities.Player;
+import com.mlb.mlb_api.repositories.PlayerRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+@Service
+public class PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    public PlayerService(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    public Iterable<Player> findAllPlayers(){
+        return playerRepository.findAll();
+    }
+
+    public Player getPlayerById(Integer id){
+        Optional<Player> optionalPlayer = playerRepository.findById(id);
+
+        if(optionalPlayer.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Player not found with id: " + id);
+        }
+//        gets the player out of the optional container and returns it to the controller
+        return optionalPlayer.get();
+    }
+
+    public Player addPlayer(Player player){
+        return playerRepository.save(player);
+    }
+
+    public Player updatePlayer(Integer id, Player updates){
+        Player playerToUpdate = getPlayerById(id);
+
+        if(!updates.getName().isEmpty()){
+            playerToUpdate.setName(updates.getName());
+        }
+        if(updates.getAge() != null){
+            playerToUpdate.setAge(updates.getAge());
+        }
+        if(updates.getRating() != null){
+            playerToUpdate.setRating(updates.getRating());
+        }
+        if(updates.getYearsOfExperience() != null){
+            playerToUpdate.setYearsOfExperience(updates.getYearsOfExperience());
+        }
+
+        return playerRepository.save(playerToUpdate);
+    }
+
+    public HashMap<String, Object> deletePlayer(Integer id){
+        HashMap<String, Object> responseMap = new HashMap<>();
+
+        Optional<Player> optionalPlayer = playerRepository.findById(id);
+
+        if(optionalPlayer.isEmpty()){
+//            if the player does not exist, this is what will be returned
+            responseMap.put("wasDeleted", false);
+            responseMap.put("playerInfo", null);
+            responseMap.put("Message", "Player not found with id: " + id);
+            return responseMap;
+        }
+
+        responseMap.put("wasDeleted", true);
+        responseMap.put("playerInfo", optionalPlayer.get());
+
+        return responseMap;
+    }
+
+}
